3b921d260a9b4bba3a057566d65359c956fcff69
S.S. Hu <sishi_hu12@hotmail.com>
2020-09-02 17:59:22 +0200

    Added layout

---
+++ app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -0,0 +1,13 @@
+package com.example.madlevel1task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.madlevel1task1.databinding.ActivityHigherLowerBinding
+
+class HigherLowerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHigherLowerBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHigherLowerBinding.inflate(layoutInflater)
+    }
+}
