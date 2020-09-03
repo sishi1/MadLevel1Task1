c98e374629b86bff25eee8240adc805dad03793f
S.S. Hu <sishi_hu12@hotmail.com>
2020-09-03 09:57:32 +0200

    Initial state set

--- app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
+++ app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -6,9 +6,30 @@ import com.example.madlevel1task1.databinding.ActivityHigherLowerBinding
 
 class HigherLowerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHigherLowerBinding
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHigherLowerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViews()
+    }
+
+    private fun updateUI() {
+        binding.IVThrow.text = getString(R.string.Throw_Label, lastThrow)
+
+        when(currentThrow) {
+            1 -> binding.dice.setImageResource(R.drawable.dice1)
+            2 -> binding.dice.setImageResource(R.drawable.dice2)
+            3 -> binding.dice.setImageResource(R.drawable.dice3)
+            4 -> binding.dice.setImageResource(R.drawable.dice4)
+            5 -> binding.dice.setImageResource(R.drawable.dice5)
+            6 -> binding.dice.setImageResource(R.drawable.dice6)
+        }
+    }
+
+    private fun initViews() {
+        updateUI()
     }
 }
